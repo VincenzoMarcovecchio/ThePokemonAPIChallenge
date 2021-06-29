b52477868adf1d4062889b049ceaddcc2de10799
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2021-06-29 13:51:32 +0100

    Updates

---
+++ precache-manifest.8f7d48635dffb3ea29a70bc3bd1fecca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f67e93ed560e888a2510f8c87666ef",
+    "url": "/ThePokemonAPIChallenge/index.html"
+  },
+  {
+    "revision": "154a205bee90eaa88199",
+    "url": "/ThePokemonAPIChallenge/static/css/main.4de2887f.chunk.css"
+  },
+  {
+    "revision": "358fdac76abd8b1a752b",
+    "url": "/ThePokemonAPIChallenge/static/js/2.5eac33e9.chunk.js"
+  },
+  {
+    "revision": "154a205bee90eaa88199",
+    "url": "/ThePokemonAPIChallenge/static/js/main.6de637b2.chunk.js"
+  },
+  {
+    "revision": "f71a29b2045627b7febc",
+    "url": "/ThePokemonAPIChallenge/static/js/runtime~main.b79203c7.js"
+  }
+]);
